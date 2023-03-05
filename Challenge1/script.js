3879a904d03c5f8cc77b1e3fa9f34a3e7d75418c
Moise Konan <mokonan99@gmail.com>
2023-03-05 17:03:55 +0000

    un de demo; jai pas finis je cherche des solution

--- Challenge1/script.js
+++ Challenge1/script.js
@@ -1,64 +1,59 @@
-let test = document.querySelectorAll('.test')
+// let test = document.querySelectorAll('.test')
 let resul = document.querySelector('.btn_result')
 let ajoutDé = document.querySelector('.btn_div')
 let blocDé = document.querySelector('#bloc_de')
 
-// console.log(test)
-
-// console.log(test)
-// tabNbr = [1,2,3,4,5,6]
-// let total = 0;
-// test.forEach(Elemen =>{
-    
-//     Elemen.addEventListener('click',()=>{
-//         nbrAléa = Math.floor(Math.random()*tabNbr.length)
-//         Elemen.textContent = nbrAléa
-
-//         total = total + nbrAléa
-//         console.log('test1',total)
-//     })
-
-// }) 
-
-// resul.addEventListener('click', ()=>{
-//     alert(total)
-//     console.log('test hors test')
-// })
-
-// Ajout de Div
+// AJOUT DES DIV
 let cptDiv = 0;
-ajoutDé.addEventListener('click', function() {
+let total = 0;
+ajoutDé.addEventListener('click', function(e) {
+    e.preventDefault()
     cptDiv++
+
+    console.log("Jia ajouter une div")
 
     if (cptDiv <= 5){
 
         var div = document.createElement('div');
         div.innerHTML = 'Click !';
-        div.className = 'de test';
-     
+        div.classList = 'de test';
+        console.log("Jia creer une div")
         blocDé.appendChild(div);
-        let test = document.querySelectorAll('.test')
+
+        var test = document.querySelectorAll('.test')
         console.log(test)
-        tabNbr = [1,2,3,4,5,6]
-        total = 0;
-        test.forEach(Elemen =>{
-            
-            Elemen.addEventListener('click',()=>{
-                nbrAléa = Math.floor(Math.random()*tabNbr.length)
-                Elemen.textContent = nbrAléa
-
-                total = total + nbrAléa
-                console.log('test2',total)
-            })
-
-        }) 
-
+        // tabNbr = [1,2,3,4,5,6]
 
     }
-    resul.addEventListener('click', ()=>{
-        alert(total)
-        console.log('test in test')
 
-    })
+    var c = 0
+    test.forEach(Elemen =>{
+        if (Elemen){
+            Elemen.addEventListener('click',()=>{
+                c++
+                console.log('le passage n° ', c)
+                if (Elemen)
+                nbrAléa = Math.trunc((Math.random()*6)+1)
+                console.log("Je genere un nbr aléa")
+                console.log('Le nbr alé: ', nbrAléa)
+                Elemen.textContent = nbrAléa
+                console.log(Elemen.textContent)
+                // console.log(eval(Elemen.textContent+2))
+                console.log('le result est ', total + Elemen.textContent)
+                total = parseInt(total) + parseInt(Elemen.textContent)
+                console.log('le total est : ',total)
+            })
+        }
+        else{
+            console.log("Code vas te tuer")
+        }
+
+    }) 
     
 }, false);
+
+resul.addEventListener('click', ()=>{
+    alert(total)
+    console.log('test in test')
+    window.location.reload()
+})
